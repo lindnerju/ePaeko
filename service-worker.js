2c421c8658195b1f815c83424e511fbaf737fb36
szaimen <42591237+szaimen@users.noreply.github.com>
2020-08-05 17:59:54 +0200

    behebe cache-reset

--- service-worker.js
+++ service-worker.js
@@ -13,10 +13,8 @@ var contentToCache = [
 ];
 
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
-      console.log('[Service Worker] Caching all files');
       return cache.addAll(contentToCache);
     })
   );
@@ -25,14 +23,25 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => {
-      console.log('[Service Worker] Fetching resource: '+e.request.url);
       return r || fetch(e.request).then((response) => {
         return caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
           cache.put(e.request, response.clone());
           return response;
         });
       });
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  var cacheKeeplist = [cacheName];
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (cacheKeeplist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
     })
   );
 });
